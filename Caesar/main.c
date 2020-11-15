9291e332471fe05a79fbfd8eefaeedfafeb4e971
Matan22g <64740256+Matan22g@users.noreply.github.com>
2020-11-15 12:45:14 +0200

    Step 1 completed

--- Caesar/main.c
+++ Caesar/main.c
@@ -4,14 +4,35 @@
 char decrepted_char(char curr_char, int decr_key)
 {
 	char new_char = 'a';
+	int num_expression = curr_char - decr_key;
+
 	if (curr_char >= 'a' && curr_char <= 'z')
-		new_char = 'a' + (curr_char - 'a' - decr_key) % 26;
+	{
+		num_expression = (num_expression - 'a');
+		if (num_expression < 0)
+			num_expression = cyclic(num_expression, 26);
+		new_char = 'a' + num_expression % 26;
+	}
+
+	else if (curr_char >= 'A' && curr_char <= 'Z')
+	{
+		num_expression = (num_expression - 'A');
+		if (num_expression < 0)
+			num_expression = cyclic(num_expression, 26);
+		new_char = 'A' + num_expression % 26;
+	}
+	else if (curr_char >= '0' && curr_char <= '9')
+	{
+		new_char = '0' + (num_expression - '0') % 10;
+
+		num_expression = (num_expression - '0');
+		if (num_expression < 0)
+			num_expression = cyclic(num_expression, 10);
+		new_char = '0' + num_expression % 10;
+	}
 	else
-		if (curr_char >= 'A' && curr_char <= 'Z')
-			new_char = 'A' + (curr_char - 'A' - decr_key) % 26;
-		else
-			if (curr_char >= '0' && curr_char <= '9')
-				new_char = '0' + (curr_char - '0' - decr_key) % 10;
+		new_char = curr_char;
+
 	return new_char;
 }
 
@@ -37,6 +58,11 @@ int excute(FILE* input_f, FILE* output_f, int key)
 	}
 }
 
+int cyclic(int to_round, int top)
+{
+	return top + to_round;
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc != 3)
@@ -54,15 +80,18 @@ int main(int argc, char* argv[])
 	}
 
 	int key = *argv[2]-'0';
-	char test = decrepted_char('d', key);
-	printf("%c", test);
+
 
 	printf("Num of argc recieved: %d\n", argc);
 	printf("key is: %d\n", key);
 	printf("File path is: %s\n", argv[1]);
 	printf("first arg is: %s\n", argv[0]);
 
+
 	excute(encypted_file, decrypted_file, key);
+
+
+	printf("mludlo check: %d\n", -3 % 26);
 
 	fclose(encypted_file);
 	fclose(decrypted_file);
