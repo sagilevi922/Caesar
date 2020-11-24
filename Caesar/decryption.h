91169448b25cc82674e21eab61333a3bdf165422
Matan22g <64740256+Matan22g@users.noreply.github.com>
2020-11-24 20:34:48 +0200

    header update

--- Caesar/decryption.h
+++ Caesar/decryption.h
@@ -22,10 +22,10 @@ typedef struct thread_arguments {
 
 char translate_char(char curr_char, int decr_key);
 int cyclic(int to_round, int top);
-DWORD WINAPI translate_file(LPVOID lpParam);
 int close_handles_proper(HANDLE file_handle);
-
-
 char* txt_file_to_str(HANDLE hFile, int start_pos, int input_size);
 HANDLE get_input_file_handle(char* input_file_name);
+HANDLE create_file_for_write();
+DWORD WINAPI translate_file(LPVOID lpParam);
+
 //#endif // __IO_THREAD_H__
